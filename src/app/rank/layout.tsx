0dced5397127141b5f95e43ab6e6795d3797991e
Lukim99 <casting.acc001@gmail.com>
2026-01-19 22:28:37 +0900

    dashboard to rank

--- src/app/rank/layout.tsx
+++ src/app/rank/layout.tsx
@@ -2,8 +2,8 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "관리자 대시보드 | 테탑하실분", // 여기에 원하는 타이틀 입력
-  description: "실시간 카카오톡 로그 저장소입니다.",
+  title: "테탑하실분 채팅 순위",
+  description: "테탑하실분 채팅 순위표를 보여주는 사이트",
 };
 
 export default function DashboardLayout({
